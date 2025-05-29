a70d5d1a7bb2737d1e5aedae9a7aac59de5f0460
Elham Bashari <elhambashari84@gmail.com>
2025-05-29 10:41:33 +0200

    navigation test paased

--- src/tests/navigation.spec.js
+++ src/tests/navigation.spec.js
@@ -1,34 +1,39 @@
 
 import { test, expect } from '@playwright/test';
 
-test.describe('Läslistan app', () => {
+test.describe('navigation mellan sidor', () => {
   test.beforeEach(async ({ page }) => {
-    
     await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/');
   });
 
-  test("Katalog button finns i första sidan", async ({ page }) => {
-    const katalogButton = page.locator('[data-testid="catalog"]');
-
-    await expect(katalogButton).toBeVisible();
-
-    await expect(katalogButton).toBeDisabled();
-  });
 
 
 
-
-
-    test('Buttons "lägg till bok" and "mina böcker" finns i första sidan och kan clicka på', async ({ page }) => {
+  test('användare kan gå mellan tre avdelningarna: "katalog", "lägg till bok" och "mina böcker"', async ({ page }) => {
+    
     const addBookButton = page.getByRole('button', { name: /lägg till bok/i });
-    const myBooksButton = page.getByRole('button', { name: /mina böcker/i });
-
-    await expect(addBookButton).toBeVisible();
-    await expect(myBooksButton).toBeVisible();
-
     await expect(addBookButton).toBeEnabled();
+    await addBookButton.click();
+
+    
+    const formTitle = page.getByText(/lägg till ny bok/i);
+    await expect(formTitle).toBeVisible({ timeout: 10000 });
+
+
+
+    const myBooksButton = page.getByRole('button', { name: /mina böcker/i });
     await expect(myBooksButton).toBeEnabled();
+    await myBooksButton.click();
+
+    const emptyMessage = page.getByText(/när du valt, kommer dina favoritböcker att visas här/i);
+    await expect(emptyMessage).toBeVisible({ timeout: 10000 });
+
+
+	
+    const katalogButton = page.locator('[data-testid="catalog"]');
+    await expect(katalogButton).toBeEnabled();
+    await katalogButton.click();
+
+    await expect(page.getByText(/katalog/i)).toBeVisible();
   });
-
-
 });
